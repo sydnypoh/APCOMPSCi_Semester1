53608aa9f107a35a6ff5ff2b40fc08d8ddaec70d
sydnypoh <sydnysoftball9@gmail.com>
2016-09-02 12:07:41 -0700

    ASCIItwo

---
+++ APLesson.01/ACSIItwo.java
@@ -0,0 +1,21 @@
+public class ACSIItwo
+{
+	public static void main(String[]args)
+	{
+		System.out.println("               ....");
+		System.out.println("           ............ ");
+		System.out.println("        X................X");
+		System.out.println("      ....X.............X...");
+		System.out.println("     ......X...........X...... ");
+		System.out.println("    ........X.........X........");
+		System.out.println("   ..........X.......X..........");
+		System.out.println("   ..........X.......X..........");
+		System.out.println("   ..........X.......X..........");
+		System.out.println("    ........X.........X........");
+		System.out.println("     ......X...........X.....");
+		System.out.println("      ....X.............X...");
+		System.out.println("        .X...............X");
+		System.out.println("           .............  ");
+		System.out.println("               ..... ");
+	}
+}
