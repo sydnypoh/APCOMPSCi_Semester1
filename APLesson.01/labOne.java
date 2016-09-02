c40bf421f6cf610103b04f055a9c49a38949641d
sydnypoh <sydnysoftball9@gmail.com>
2016-09-02 11:39:02 -0700

    ASCIIone

---
+++ APLesson.01/labOne.java
@@ -0,0 +1,20 @@
+public class labOne
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                       .          ");
+		System.out.println("                      .M          ");
+		System.out.println("                     ,MM          ");
+		System.out.println("                     MM:          ");
+		System.out.println("             .       YMM,         ");
+		System.out.println("             M       'MMM,       ,M");
+		System.out.println("             M.       ,MMM      ,M ");
+		System.out.println("             MM,      ,MMM     ,MM' ");
+		System.out.println("             'MMM,   MMN     ,MMMM ");
+		System.out.println("             .MMMMMM.MMMMM.MMMMMM.");
+		System.out.println("             MMMMMMMMMMMMMMMMMMMMM");
+		System.out.println("             'MMMMMMMMMMMMMMMMMMM'");
+		System.out.println("              ''''''''''''''''''' ");
+
+	}
+}
