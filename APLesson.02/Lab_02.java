7b38bc131884f333edd2c0c09ae9947fe4963095
sydnypoh <sydnysoftball9@gmail.com>
2016-09-07 12:05:14 -0700

    Lab 2

---
+++ APLesson.02/Lab_02.java
@@ -0,0 +1,31 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 9;
+		int b = 6;
+		int answer = a * b;
+		String one = "Candy ";
+		String two = "Sweet ";
+		String three = "Cane ";
+		String four = "Lane ";
+		String five = "North ";
+		String six = "Pole ";
+		String seven = "Antartica ";
+		int c = 12345;
+		int l = 3;
+		int w = 4;
+		int h = 5;
+		int sone = l * w;
+		int stwo = l * h;
+		int sthree = w * h;
+		int half = sone + stwo + sthree;
+		int whole = 2 * half;
+		
+		
+		System.out.println("Nine times six equals " + answer);
+		System.out.println("My address is " + one + two + three + four);
+		System.out.println(five + six + seven +c);
+		System.out.println("The surface are of your ractangle is " + whole);
+	}
+}
