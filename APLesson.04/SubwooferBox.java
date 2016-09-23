36c236fcfb37dfa7bec2e109188992e5d1984fe3
sydnypoh <sydnysoftball9@gmail.com>
2016-09-23 12:09:38 -0700

    Subwoofer Box

---
+++ APLesson.04/SubwooferBox.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SubwooferBox
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		SubwooferBox area = new SubwooferBox();
+		
+		System.out.println("Subwoofer Box is here. Enter everything in inches. Please enter the height of your space:");
+		double h = keyboard.nextDouble();
+		System.out.println("Please enter the length of your space:");
+		double l = keyboard.nextDouble();
+		System.out.println("Please enter the width of you space:");
+		double w = keyboard.nextDouble();
+		
+		double ft = area.calcVol(h, l, w);
+		
+		System.out.println("The area of you space in cubic feet is " + ft + "ft^3");
+	}
+	
+	public double calcVol(double h, double l, double w)
+	{
+		return ((h*l*w)/1728);
+	}
+}
