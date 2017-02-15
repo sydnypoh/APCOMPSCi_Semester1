00fe2ef4bd9383d6f82cc5cc47635c2439f06fc0
sydnypoh <sydnysoftball9@gmail.com>
2017-02-15 11:34:02 -0800

    Lab_13.2

--- APLesson.13/TicketDriver.java
+++ APLesson.13/TicketDriver.java
@@ -6,8 +6,8 @@ public class TicketDriver
 		Ticket adv = new Advance(10);
 		Ticket sadv = new StudentAdvance(6);
 		
-		System.out.println(wu);
-		System.out.println(adv);
+		System.out.println(wu + "\n");
+		System.out.println(adv + "\n");
 		System.out.println(sadv);
 	}
 }
