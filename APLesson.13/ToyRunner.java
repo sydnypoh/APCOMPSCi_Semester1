fe032c0b68084aeeec64c5a2ec6af627c5728990
sydnypoh <sydnysoftball9@gmail.com>
2017-04-17 10:58:34 -0700

    Lab_13

--- APLesson.13/ToyRunner.java
+++ APLesson.13/ToyRunner.java
@@ -7,7 +7,7 @@ public class ToyRunner
 		String toy = "Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF";
 		toystore store = new toystore(toy);
 		System.out.println(store);
-		System.out.println(store.getMostFrequentToy());
-		System.out.println(store.getMostFrequentType());
+		System.out.println("\nMost Frequent Toy: " + store.getMostFrequentToy());
+		System.out.println("Most Frequent Type of Toy: " +store.getMostFrequentType());
 	}
 }
